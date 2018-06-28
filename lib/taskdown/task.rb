15f7e5cb82cee6b026766b49bd82f5b3558ea3a8
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-06-28 17:23:42 +0200

    Parse date attributes

--- lib/taskdown/task.rb
+++ lib/taskdown/task.rb
@@ -1,12 +1,12 @@
 class Task
-  attr_reader :name, :finished
+  attr_reader :name, :finished, :attributes
 
   # Create many task from given filepath
   # @param filepath <String> filepath of readable markdown file
   def self.from_file filepath
     tasks = []
 
-    File.read(filepath).scan(/- \[( |X)\] (.*)/).each do |data|
+    File.read(filepath).scan(/- \[( |X)\] (.+)/).each do |data|
       task = Task.new data[1], (data[0] == 'X')
       tasks << task
       yield if block_given?
@@ -15,9 +15,30 @@ class Task
     return tasks
   end
 
-  def initialize(name, finished)
+  def initialize(name, finished, attributes = {})
     @name = name
     @finished = finished
+    @attributes = attributes
+
+    parse_attributes!
+  end
+
+  private
+
+  def parse_attributes!
+    @name.scan(/@([a-z]*)=(.*)/).each do |data|
+      # insert as attribute
+      key = data[0].to_sym
+      value = data[1]
+      # create
+      @name.gsub! "@#{key}=#{value}", ''
+      @name.chomp!(' ')
+      begin
+        value = Date.parse value
+      rescue Exception => e
+      end
+      @attributes[key] = value
+    end
   end
 
 end
