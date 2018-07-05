de21ebce6a9b90b164b288d7264a7643dd53a021
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-07-05 13:17:06 +0200

    Reorganise test

--- spec/todown_spec.rb
+++ spec/todown_spec.rb
@@ -2,42 +2,8 @@ require 'date'
 
 RSpec.describe Todown do
 
-  it "read tasks" do
-    expected_tasks = [
-      Task.new('Milk', true),
-      Task.new('Sugar', false),
-      Task.new('Chocolate', false, {due: Date.new(2019, 2, 14)}),
-    ]
-
-    tasks = Task.from_file("sample.markdown")
-
-    3.times do |i|
-      expect(tasks[i].name).to eq(expected_tasks[i].name)
-      expect(tasks[i].finished).to eq(expected_tasks[i].finished)
-      expect(tasks[i].attributes).to eq(expected_tasks[i].attributes)
-    end
-  end
-
   it "has a version number" do
     expect(Todown::VERSION).not_to be nil
   end
 
-  it "Should parse date attribute" do
-    task = Task.new "Hello @due=1992-12-28"
-    expect(task.name).to eq('Hello')
-    expect(task.attributes[:due]).to eq(Date.new 1992, 12, 28)
-  end
-
-  it "Should parse string attribute" do
-    task = Task.new "Hello @due=today"
-    expect(task.name).to eq('Hello')
-    expect(task.attributes[:due]).to eq('today')
-  end
-
-  it "Should parse with string attribute & date attribute" do
-    task = Task.new "Make something great @due=today @tag=world"
-    expect(task.name).to eq('Make something great')
-    expect(task.attributes[:due]).to eq('today')
-    expect(task.attributes[:tag]).to eq('world')
-  end
 end
