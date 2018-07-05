ff54450b5c9b62fd5816128d76b900ff2707a66e
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-07-05 17:00:03 +0200

    Improve attribute display display

--- lib/todown.rb
+++ lib/todown.rb
@@ -9,14 +9,44 @@ module Todown
   # @param tasks [Array<Task>]
   # @return [Terminal::Table]
   def self.task_to_table tasks
+    return Terminal::Table.new(Todown.task_to_hash_table(tasks))
+  end
+
+  # Manipulate data to transform in the expected format for `Terminal::Table`
+  #
+  # @param tasks [Array<Task>]
+  # @return [Hash<Symbol, Array>]
+  def self.task_to_hash_table tasks
     rows = []
-    headings = ['', 'Name', 'Attributes']
+    attributes_keys = []
 
     tasks.each do |task|
-      status = task.finished ? 'X' : ''
-      rows << [status, task.name, task.attributes.to_s]
+      task.attributes.keys.each {|key| attributes_keys << key.to_s }
     end
 
-    return Terminal::Table.new rows: rows, headings: headings
+    attributes_keys.uniq!
+
+    headings = ['', 'Name'].concat attributes_keys
+
+    tasks.each do |task|
+      row = []
+      row << (task.finished ? 'X' : '')
+      row << task.name
+
+      # Insert attributes in the same order
+      attributes_keys.each do |attribute_key|
+        attribute_value = task.attributes[attribute_key.to_sym]
+        if attribute_value
+          row << attribute_value.to_s
+        else
+          row << ''
+        end
+      end
+
+      rows << row
+    end
+
+    return {rows: rows, headings: headings }
   end
+
 end
--- spec/todown_spec.rb
+++ spec/todown_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Todown do
     expect(Todown::VERSION).not_to be nil
   end
 
+  it "convert tasks to table" do
+    tasks  = [
+      Task.new("One @due=2018-01-01"),
+      Task.new("Two @until=dawn"),
+      Task.new("Two b @until=dawn"),
+      Task.new("Three"),
+    ]
+
+    hash_data = Todown.task_to_hash_table(tasks)
+
+
+    expect(hash_data[:headings]).to eq ['', 'Name', 'due', 'until']
+    expect(hash_data[:rows]).to eq [
+      ['', 'One', '2018-01-01', ''],
+      ['', 'Two', '', 'dawn'],
+      ['', 'Two b', '', 'dawn'],
+      ['', 'Three', '', ''],
+    ]
+  end
+
 end
