a65aafd906a41d395e7ee3bd5293fc677af728cb
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-07-05 13:14:23 +0200

    Move cli logic in module

--- lib/todown.rb
+++ lib/todown.rb
@@ -1,6 +1,22 @@
 require "todown/version"
 require "todown/task"
+require 'terminal-table'
 
 module Todown
-  # Your code goes here...
+
+  # Create a `Terminal::Table` from many task
+  #
+  # @param tasks [Array<Task>]
+  # @return [Terminal::Table]
+  def self.task_to_table tasks
+    rows = []
+    headings = ['', 'Name', 'Attributes']
+
+    tasks.each do |task|
+      status = task.finished ? 'X' : ''
+      rows << [status, task.name, task.attributes.to_s]
+    end
+
+    return Terminal::Table.new rows: rows, headings: headings
+  end
 end
